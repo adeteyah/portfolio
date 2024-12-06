a639fd57b4731523654342894cae43afeb81db21
Adeteyah <adeteyah@gmail.com>
2024-12-06 00:50:23 +0000

    Dec 6, 2024, 7:50 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="flex items-center gap-4 lg:gap-8">
-    <div class="bg-red-100"></div>
-    <div class="bg-red-200 flex-1">
-      <h1 class="uppercase min-w-[1300px]:text-6xl text-7xl">
+  <div class="flex flex-col lg:flex-row items-center gap-4 lg:gap-8">
+    <div class="bg-red-100">
+      <IconsAditiyaRachman />
+    </div>
+    <div class="bg-red-200">
+      <h1 class="uppercase lg:text-7xl">
         I’m a <span class="font-semibold">UI/UX Designer</span> and
         <span class="font-semibold">Full-stack Developer</span>
       </h1>
