a0af4fdd7d879fa5c2b458adc5008ba7fe25c714
Adeteyah <adeteyah@gmail.com>
2024-12-06 01:15:25 +0000

    Dec 6, 2024, 8:15 AM

--- components/ui/Navbar.vue
+++ components/ui/Navbar.vue
@@ -7,9 +7,9 @@
       >
     </NuxtLink>
     <div class="flex gap-4 md:gap-5 lg:gap-6">
-      <NuxtLink to="/">About</NuxtLink>
-      <NuxtLink to="/">Work</NuxtLink>
-      <NuxtLink to="/">Contact</NuxtLink>
+      <NuxtLink to="/about">About</NuxtLink>
+      <NuxtLink to="/work">Work</NuxtLink>
+      <NuxtLink to="/contact">Contact</NuxtLink>
     </div>
   </div>
 </template>
