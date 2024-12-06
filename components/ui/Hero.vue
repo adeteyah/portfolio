e90e825c6e5a3b1a8f8c92f4f6b4ccf349c1295a
Adeteyah <adeteyah@gmail.com>
2024-12-06 06:02:27 +0000

    Dec 6, 2024, 1:02 PM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -1,33 +1,3 @@
 <template>
-  <img
-    class="w-full h-full transition-opacity duration-300"
-    :src="imageSrc"
-    alt="Aditiya Rachman"
-    @mouseover="handleHover(true)"
-    @mouseleave="handleHover(false)"
-  />
+  <h1>Test</h1>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      imageSrc: "/LogoEnabled.svg", // Correct path for the public directory
-    };
-  },
-  methods: {
-    handleHover(isHovered) {
-      this.imageSrc = isHovered
-        ? "/LogoHover.svg" // Correct path for hover image
-        : "/LogoEnabled.svg"; // Default path
-    },
-  },
-};
-</script>
-
-<style>
-/* Add transition for a seamless effect */
-img {
-  transition: opacity 0.3s ease-in-out;
-}
-</style>
