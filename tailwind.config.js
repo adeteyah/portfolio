754a728bc87824bfd04f80af6f2259b6f52533e7
Adeteyah <adeteyah@gmail.com>
2024-12-06 00:44:15 +0000

    Dec 6, 2024, 7:44 AM

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+    },
   },
   plugins: [],
 };
