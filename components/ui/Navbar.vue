d8209ab1c6582ee1bd7909d758e5e637b1978600
Adeteyah <adeteyah@gmail.com>
2024-12-06 01:03:50 +0000

    Dec 6, 2024, 8:03 AM

--- components/ui/Navbar.vue
+++ components/ui/Navbar.vue
@@ -3,7 +3,7 @@
     <NuxtLink to="/" class="font-semibold hover:no-underline">
       Aditiya Rachman
     </NuxtLink>
-    <div class="flex gap-4">
+    <div class="flex gap-4 md:gap-5 lg:gap-6">
       <NuxtLink to="/">About</NuxtLink>
       <NuxtLink to="/">Work</NuxtLink>
       <NuxtLink to="/">Contact</NuxtLink>
