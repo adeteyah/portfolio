790cf422827c345cbecb6a1307ec752d34564993
Adeteyah <adeteyah@gmail.com>
2024-12-06 06:54:48 +0000

    Dec 6, 2024, 1:54 PM

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,13 +1,7 @@
 <template>
-  <div>
-    
-  </div>
+  <div>test</div>
 </template>
 
-<script lang="ts" setup>
+<script lang="ts" setup></script>
 
-</script>
-
-<style>
-
-</style>
+<style></style>
