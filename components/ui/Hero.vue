9ca2f40e93ce37568063bbc29b902e9aa1bc2c8c
Adeteyah <adeteyah@gmail.com>
2024-12-06 00:25:28 +0000

    Dec 6, 2024, 7:25 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flex items-center gap-4 lg:gap-8">
-    <div class="bg-red-100">
-      <IconsAditiyaRachman />
-    </div>
+    <div class="bg-red-100"></div>
     <div class="bg-red-200 flex-1">
       <h1 class="uppercase min-w-[1300px]:text-6xl text-7xl">
         I’m a <span class="font-semibold">UI/UX Designer</span> and
--- components/ui/Navbar.vue
+++ components/ui/Navbar.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="flex justify-between md:text-base">
-    <NuxtLink to="/" class="font-semibold hover:no-underline">
-      Aditiya Rachman
-    </NuxtLink>
+    <div class="flex gap-4">
+      <IconsAditiyaRachman />
+      <NuxtLink to="/" class="font-semibold hover:no-underline">
+        Aditiya Rachman
+      </NuxtLink>
+    </div>
+
     <div class="flex gap-4">
       <NuxtLink to="/">About</NuxtLink>
       <NuxtLink to="/">Work</NuxtLink>
