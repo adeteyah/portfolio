cae783f80958d161110c8bfdf7acdbe98c13b90a
Adeteyah <adeteyah@gmail.com>
2024-12-06 06:12:28 +0000

    Dec 6, 2024, 1:12 PM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1 class="text-4xl md:text-5xl lg:text-6xl">
-    FREELANCE DESIGNER & DEVELOPER
-  </h1>
+  <div class="flex justify-center py-24">
+    <h1 class="text-4xl md:text-5xl lg:text-6xl">
+      FREELANCE <span class="font-semibold">DESIGNER</span> &
+      <span class="font-semibold">DEVELOPER</span>
+    </h1>
+  </div>
 </template>
