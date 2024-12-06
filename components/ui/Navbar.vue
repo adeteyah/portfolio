714ed7d3c903769c10e68882b5800aecb55958b6
Adeteyah <adeteyah@gmail.com>
2024-12-06 01:13:13 +0000

    Dec 6, 2024, 8:13 AM

--- components/ui/Navbar.vue
+++ components/ui/Navbar.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex justify-between md:text-base">
-    <NuxtLink to="/" class="font-semibold hover:no-underline">
-      Aditiya Rachman
+    <NuxtLink to="/" class="group font-semibold hover:no-underline">
+      A<span class="lg:text-white group-hover:text-black">ditiya</span> R<span
+        class="lg:text-white group-hover:text-black"
+        >achman</span
+      >
     </NuxtLink>
     <div class="flex gap-4 md:gap-5 lg:gap-6">
       <NuxtLink to="/">About</NuxtLink>
