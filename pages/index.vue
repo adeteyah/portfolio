d347c7dacf7fe78e8c37f25b641d06e5d61a8a1a
Adeteyah <adeteyah@gmail.com>
2024-12-06 07:39:29 +0000

    Dec 6, 2024, 2:39 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="grid lg:grid-cols-2 gap-4 lg:gap-8">
-    <div v-for="work in works" :key="work.id">
+  <div class="grid lg:grid-cols-4 gap-4 lg:gap-8">
+    <div v-for="work in works.slice(0, 4)" :key="work.id">
       <nuxt-link class="no-hover" :to="`/work/${work.id}`">
-        <img class="rounded-md" :src="work.image" :alt="work.title" />
+        <div class="relative w-full h-[300px]">
+          <img
+            class="rounded object-cover w-full h-full"
+            :src="work.image"
+            :alt="work.title"
+          />
+        </div>
         <h3 class="text-3xl py-4 font-semibold">{{ work.title }}</h3>
         <hr class="opacity-10" />
         <div class="flex justify-between py-4 opacity-50">
