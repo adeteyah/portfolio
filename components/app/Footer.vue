840b4e7f0503a349a08b3cf5c75aa03964fc21df
Adeteyah <adeteyah@gmail.com>
2024-12-06 01:44:56 +0000

    Dec 6, 2024, 8:44 AM

--- components/app/Footer.vue
+++ components/app/Footer.vue
@@ -4,10 +4,10 @@
       <IconsAditiyaRachman />
     </div>
     <div class="flex gap-4 md:gap-5 lg:gap-6 py-8 lg:justify-end">
-      <a target="_blank" href="/contact">Medium</a>
-      <a target="_blank" href="/about">Twitter</a>
-      <a target="_blank" href="/work">Instagram</a>
-      <a target="_blank" href="/work">GitHub</a>
+      <a target="_blank" href="https://adeteyah.medium.com/">Medium</a>
+      <a target="_blank" href="https://x.com/adeteyah">Twitter</a>
+      <a target="_blank" href="https://www.instagram.com/adeteyah">Instagram</a>
+      <a target="_blank" href="https://github.com/adeteyah">GitHub</a>
     </div>
     <div class="flex justify-between opacity-50">
       <small>&copy; 2024</small>
