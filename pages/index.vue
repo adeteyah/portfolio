0d973d1a10356fb07c767aa46a88408680955d89
Adeteyah <adeteyah@gmail.com>
2024-12-06 08:26:03 +0000

    Dec 6, 2024, 3:26 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="grid lg:grid-cols-4 -mx-4">
+  <div class="grid lg:grid-cols-3 -mx-4">
     <div
       class="px-4 pt-4 hover:bg-gray rounded transition-colors duration-500"
-      v-for="work in works.slice(0, 4)"
+      v-for="work in works.slice(0, 3)"
       :key="work.id"
     >
       <nuxt-link class="group no-hover" :to="`/work/${work.id}`">
         <div
-          class="rounded relative w-full h-[300px] p-4 group-hover:p-0 bg-gray transition-all duration-500"
+          class="rounded relative w-full h-[400px] p-4 group-hover:p-0 bg-gray transition-all duration-500"
         >
           <img
             class="rounded object-cover w-full h-full"
