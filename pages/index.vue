a5961eed9e98142d189ceb09405539e2655b4de9
Adeteyah <adeteyah@gmail.com>
2024-12-06 07:32:18 +0000

    Dec 6, 2024, 2:32 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>Test</div>
+  <div class="grid lg:grid-cols-2 gap-4 lg:gap-8">
+    <div v-for="work in works" :key="work.id">
+      <nuxt-link class="no-hover" :to="`/work/${work.id}`">
+        <img class="rounded-md" :src="work.image" :alt="work.title" />
+        <h3 class="text-3xl py-4 font-semibold">{{ work.title }}</h3>
+        <hr class="opacity-10" />
+        <div class="flex justify-between py-4 opacity-50">
+          <p>{{ work.year }}</p>
+          <p>{{ work.year }}</p>
+        </div>
+      </nuxt-link>
+    </div>
+  </div>
 </template>
-
-<script lang="ts" setup></script>
-
-<style></style>
+<script>
+import { works } from "~/data/work.js";
+export default {
+  data() {
+    return {
+      works,
+    };
+  },
+};
+</script>
