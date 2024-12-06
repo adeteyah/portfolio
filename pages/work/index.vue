0cbc7acafae3d5806dd475be15534525eed741a6
Adeteyah <adeteyah@gmail.com>
2024-12-06 07:17:23 +0000

    Dec 6, 2024, 2:17 PM

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="grid grid-cols-2 gap-4 lg:gap-8">
+  <div class="grid lg:grid-cols-2 gap-4 lg:gap-8">
     <div v-for="work in works" :key="work.id">
-      <nuxt-link :to="`/work/${work.id}`">
+      <nuxt-link class="no-hover" :to="`/work/${work.id}`">
         <img class="rounded-md" :src="work.image" :alt="work.title" />
         <h3 class="text-3xl py-4 font-semibold">{{ work.title }}</h3>
+        <hr class="opacity-10" />
+        <p></p>
       </nuxt-link>
     </div>
   </div>
