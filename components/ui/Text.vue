29a5292f2f81e4e40d46ca24527e793d27a1eec1
Adeteyah <adeteyah@gmail.com>
2024-12-06 20:13:15 +0000

    Dec 7, 2024, 3:13 AM

--- components/ui/Text.vue
+++ components/ui/Text.vue
@@ -3,7 +3,7 @@
     <div class="indent-28 opacity-50 text-xl md:text-2xl lg:text-3xl">
       [ <slot name="title" /> ]
     </div>
-    <div class="indent-28 text-3xl md:text-4xl lg:text-5xl">
+    <div class="indent-28 text-3xl md:text-4xl lg:text-5xl lg:leading-relaxed">
       <slot name="text" />
     </div>
   </div>
