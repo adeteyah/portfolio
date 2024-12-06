36430ea026b5d3a569309e9b8b5d16912678c933
Adeteyah <adeteyah@gmail.com>
2024-12-06 08:00:04 +0000

    Dec 6, 2024, 3:00 PM

--- components/ui/Navbar.vue
+++ components/ui/Navbar.vue
@@ -1,19 +1,39 @@
 <template>
-  <div class="flex justify-between md:text-base">
-    <NuxtLink to="/" class="group font-semibold hover:no-underline">
-      A<span
-        class="lg:text-white group-hover:text-black transition-colors duration-500"
-        >ditiya</span
-      >
-      R<span
-        class="lg:text-white group-hover:text-black transition-colors duration-1000"
-        >achman</span
-      >
-    </NuxtLink>
-    <div class="flex gap-4 md:gap-5 lg:gap-6">
-      <NuxtLink to="/work">Work</NuxtLink>
-      <NuxtLink to="/about">About</NuxtLink>
-      <NuxtLink to="/contact">Contact</NuxtLink>
+  <nav>
+    <div class="flex justify-between md:text-base">
+      <NuxtLink to="/" class="group font-semibold hover:no-underline">
+        A<span
+          class="lg:text-white group-hover:text-black transition-colors duration-500"
+          >ditiya</span
+        >
+        R<span
+          class="lg:text-white group-hover:text-black transition-colors duration-1000"
+          >achman</span
+        >
+      </NuxtLink>
+      <div class="flex gap-4 md:gap-5 lg:gap-6">
+        <NuxtLink to="/work" :class="{ 'blur-[1px]': route.path !== '/work' }">
+          Work
+        </NuxtLink>
+        <NuxtLink
+          to="/about"
+          :class="{ 'blur-[1px]': route.path !== '/about' }"
+        >
+          About
+        </NuxtLink>
+        <NuxtLink
+          to="/contact"
+          :class="{ 'blur-[1px]': route.path !== '/contact' }"
+        >
+          Contact
+        </NuxtLink>
+      </div>
     </div>
-  </div>
+  </nav>
 </template>
+
+<script setup>
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+</script>
