46d517ed159591da882bceccffb588f56e281a6d
Adeteyah <adeteyah@gmail.com>
2024-12-06 15:39:36 +0000

    Dec 6, 2024, 10:39 PM

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>Test</h1>
     <div class="grid lg:grid-cols-2 gap-4 lg:gap-8">
-      <div v-for="item in work" :key="item.id">
+      <div v-for="item in workData" :key="item.id">
         <nuxt-link class="group no-hover" :to="`/work/${item.id}`">
           <div
             class="relative w-full h-[600px] bg-gray p-8 flex items-center group-hover:bg-black transition-colors duration-500"
@@ -32,5 +32,6 @@ definePageMeta({
   title: "My home page",
 });
 
-const work = work;
+// Rename the imported variable to avoid collision
+const workData = work;
 </script>
