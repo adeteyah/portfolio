f061fc3476b660c3d78b3eee3567aa0d2575119c
Adeteyah <adeteyah@gmail.com>
2024-12-06 08:02:58 +0000

    Dec 6, 2024, 3:02 PM

--- components/ui/Navbar.vue
+++ components/ui/Navbar.vue
@@ -1,33 +1,28 @@
 <template>
-  <nav>
-    <div class="flex justify-between md:text-base">
-      <NuxtLink to="/" class="group font-semibold hover:no-underline">
-        A<span
-          class="lg:text-white group-hover:text-black transition-colors duration-500"
-          >ditiya</span
-        >
-        R<span
-          class="lg:text-white group-hover:text-black transition-colors duration-1000"
-          >achman</span
-        >
+  <nav class="relative flex justify-between md:text-base">
+    <NuxtLink to="/" class="group font-semibold hover:no-underline">
+      A<span
+        class="lg:text-white group-hover:text-black transition-colors duration-500"
+        >ditiya</span
+      >
+      R<span
+        class="lg:text-white group-hover:text-black transition-colors duration-1000"
+        >achman</span
+      >
+    </NuxtLink>
+    <div class="flex gap-4 md:gap-5 lg:gap-6">
+      <NuxtLink to="/work" :class="{ 'blur-[1px]': route.path !== '/work' }">
+        Work
       </NuxtLink>
-      <div class="flex gap-4 md:gap-5 lg:gap-6">
-        <NuxtLink to="/work" :class="{ 'blur-[1px]': route.path !== '/work' }">
-          Work
-        </NuxtLink>
-        <NuxtLink
-          to="/about"
-          :class="{ 'blur-[1px]': route.path !== '/about' }"
-        >
-          About
-        </NuxtLink>
-        <NuxtLink
-          to="/contact"
-          :class="{ 'blur-[1px]': route.path !== '/contact' }"
-        >
-          Contact
-        </NuxtLink>
-      </div>
+      <NuxtLink to="/about" :class="{ 'blur-[1px]': route.path !== '/about' }">
+        About
+      </NuxtLink>
+      <NuxtLink
+        to="/contact"
+        :class="{ 'blur-[1px]': route.path !== '/contact' }"
+      >
+        Contact
+      </NuxtLink>
     </div>
   </nav>
 </template>
