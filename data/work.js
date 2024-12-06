17c1e872e75295e27214d014d8735b552af3a52c
Adeteyah <adeteyah@gmail.com>
2024-12-06 07:14:18 +0000

    Dec 6, 2024, 2:14 PM

--- data/work.js
+++ data/work.js
@@ -12,7 +12,7 @@ export const works = [
     id: 2,
     title: "Design Beta",
     image:
-      "https://assets.awwwards.com/awards/submissions/2024/02/65c3a43c2f798911476445.jpg",
+      "https://assets.awwwards.com/awards/avatar/222913/665dc853c0cd3010379460.jpg",
     techstack: ["Nuxt.js", "Tailwind CSS"],
     year: 2022,
     details: "A responsive design portfolio template.",
