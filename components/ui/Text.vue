26e55d34e08bec9855f729ddec175e67a55d8471
Adeteyah <adeteyah@gmail.com>
2024-12-06 20:25:25 +0000

    Dec 7, 2024, 3:25 AM

--- components/ui/Text.vue
+++ components/ui/Text.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="grid gap-11">
-    <div class="indent-28 opacity-50 text-xl md:text-2xl lg:text-3xl">
+    <div class="indent-28 opacity-50 text-xl md:text-2xl lg:text-3xl uppercase">
       [ <slot name="title" /> ]
     </div>
     <div class="indent-28 text-3xl md:text-4xl lg:text-5xl lg:leading-relaxed">
