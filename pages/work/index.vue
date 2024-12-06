5d6a0815c6f4462848fc53387ba2cba5486642eb
Adeteyah <adeteyah@gmail.com>
2024-12-06 07:08:00 +0000

    Dec 6, 2024, 2:08 PM

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <div v-for="work in works" :key="work.id">
-    <img :src="work.image" :alt="work.title" />
-    <h2>{{ work.title }}</h2>
-    <p><strong>Tech Stack:</strong> {{ work.techstack.join(", ") }}</p>
-    <p><strong>Year:</strong> {{ work.year }}</p>
-    <p>{{ work.details }}</p>
-    <nuxt-link :to="`/work/${work.id}`">View Details</nuxt-link>
+  <div class="grid grid-cols-2 gap-4 lg:gap-12">
+    <div v-for="work in works" :key="work.id">
+      <img :src="work.image" :alt="work.title" />
+      <h2>{{ work.title }}</h2>
+      <p><strong>Tech Stack:</strong> {{ work.techstack.join(", ") }}</p>
+      <p><strong>Year:</strong> {{ work.year }}</p>
+      <p>{{ work.details }}</p>
+      <nuxt-link :to="`/work/${work.id}`">View Details</nuxt-link>
+    </div>
   </div>
 </template>
 <script>
