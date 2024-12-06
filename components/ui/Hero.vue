476a7afa269ba8abbe7fa48f47fa13cc18b40831
Adeteyah <adeteyah@gmail.com>
2024-12-06 01:09:30 +0000

    Dec 6, 2024, 8:09 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -1,7 +1,7 @@
 <template>
-  <h1 class="uppercase text-6xl md:text-7xl lg:text-8xl">
+  <h1 class="uppercase text-4xl md:text-6xl lg:text-8xl">
     I’m a
-    <span class="font-bold">UI/UX Designer</span> &
-    <span class="font-bold">Full-stack Developer</span>
+    <span class="font-bold">Designer</span> &
+    <span class="font-bold">Developer</span>
   </h1>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="max-w-[420px] md:max-w-[720px] lg:max-w-[1920px] mx-auto">
-    <div class="px-8 md:px-24 lg:px-40 py-11">
-      <div class="grid gap-4 lg:gap-11">
-        <AppHeader />
-        <slot />
-        <AppFooter />
-      </div>
+  <div class="px-8 md:px-24 lg:px-40 py-11">
+    <div class="grid gap-4 lg:gap-11">
+      <AppHeader />
+      <slot />
+      <AppFooter />
     </div>
   </div>
 </template>
