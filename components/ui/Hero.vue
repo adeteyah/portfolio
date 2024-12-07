102216927d64f4dc0da14239b9b041438f3c76af
Adeteyah <adeteyah@gmail.com>
2024-12-07 23:05:11 +0000

    Dec 8, 2024, 6:05 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="flex justify-center py-24">
-    <h1 class="text-4xl md:text-5xl lg:text-6xl">
-      I'm Dete, and I turn design briefs into visual bangers. For real - I'm not
-      the best developer but I take pride in my design. Your project should be
-      next - let's create something great!
+    <h1
+      class="text-4xl leading-relaxed md:text-5xl md:leading-relaxed lg:text-6xl lg:leading-relaxed"
+    >
+      I’m Dete, the rare mix of a code wizard and a design powerhouse. I don’t
+      just create visuals—I craft experiences that captivate and deliver.
+      Whether it’s sleek designs or flawless functionality, I’ve got you
+      covered. Let’s team up and build something extraordinary!
       <span class="font-semibold">DESIGNER</span> &
       <span class="font-semibold">DEVELOPER</span>
     </h1>
