1e270021ff4acc3be1fe5c237165014182cb7dca
Adeteyah <adeteyah@gmail.com>
2024-12-07 22:59:35 +0000

    Dec 8, 2024, 5:59 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="flex justify-center py-24">
     <h1 class="text-4xl md:text-5xl lg:text-6xl">
-      FREELANCE <span class="font-semibold">DESIGNER</span> &
+      I'm Dete, and I turn design briefs into visual bangers. For real - I'm not
+      the best developer but I take pride in my design. Your project should be
+      next - let's create something great!
+      <span class="font-semibold">DESIGNER</span> &
       <span class="font-semibold">DEVELOPER</span>
     </h1>
   </div>
