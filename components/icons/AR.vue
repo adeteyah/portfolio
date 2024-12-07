1d27c30f7fb8c2ed81a5fe37f309428d190a550a
Adeteyah <adeteyah@gmail.com>
2024-12-07 23:22:29 +0000

    Dec 8, 2024, 6:22 AM

--- components/icons/AR.vue
+++ components/icons/AR.vue
@@ -1,8 +1,13 @@
 <template>
   <NuxtImg
-    class="w-full h-full opacity-15"
-    src="/public/Logo.svg"
+    class="svg-icon w-full h-full opacity-15"
+    src="Logo.svg"
     alt="Aditiya
   Rachman"
   />
 </template>
+<style scoped>
+.svg-icon path {
+  @apply fill-white bg-[red];
+}
+</style>
