7857fcd4516d2415c75d82e61970d1372226d52e
Adeteyah <adeteyah@gmail.com>
2024-12-07 22:35:35 +0000

    Dec 8, 2024, 5:35 AM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxtjs/tailwindcss", "@nuxt/fonts", "@nuxt/image"],
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@nuxtjs/color-mode",
+  ],
   css: ["~/assets/css/main.css"],
   devServer: {
     host: "localhost",
--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,13 @@
 <template>
+  <div>
+    <h1>Color mode: {{ $colorMode.value }}</h1>
+    <select v-model="$colorMode.preference">
+      <option value="system">System</option>
+      <option value="light">Light</option>
+      <option value="dark">Dark</option>
+      <option value="sepia">Sepia</option>
+    </select>
+  </div>
   <section>
     <div class="grid lg:grid-cols-3 -mx-4">
       <div
@@ -44,4 +53,23 @@
 <script setup>
 import work from "~/assets/data/work.js";
 const workData = work;
+
+const colorMode = useColorMode();
+
+console.log(colorMode.preference);
 </script>
+
+<style>
+body {
+  background-color: #fff;
+  color: rgba(0, 0, 0, 0.8);
+}
+.dark-mode body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia-mode body {
+  background-color: #f1e7d0;
+  color: #433422;
+}
+</style>
