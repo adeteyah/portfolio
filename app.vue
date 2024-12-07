be8bc3fd2ae97736d8b55132765679c0d75bb124
Adeteyah <adeteyah@gmail.com>
2024-12-07 22:49:10 +0000

    Dec 8, 2024, 5:49 AM

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <Lenis :options="options" root>
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </Lenis>
 </template>
+<script setup>
+const options = {
+  duration: 1.2,
+  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+  orientation: "horizontal",
+  gestureOrientation: "vertical",
+};
+</script>
