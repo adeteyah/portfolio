3ae49bd73173ec32497d50bdfb3afa1dec5081f8
Adeteyah <adeteyah@gmail.com>
2024-12-07 23:24:05 +0000

    Dec 8, 2024, 6:24 AM

--- components/icons/AR.vue
+++ components/icons/AR.vue
@@ -1,13 +1,37 @@
 <template>
-  <NuxtImg
-    class="svg-icon w-full h-full opacity-15"
-    src="Logo.svg"
-    alt="Aditiya
-  Rachman"
-  />
+  <svg
+    width="240"
+    height="264"
+    viewBox="0 0 240 264"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clip-path="url(#clip0_413_4245)">
+      <path class="shape1" d="M96 264H144L48 0H0L96 264Z" />
+      <path class="shape2" d="M144 132H96L192 264H240L144 132Z" />
+      <path class="shape3" d="M96 0H48L144 132H192L96 0Z" />
+      <path class="shape4" d="M0 0H48V264H0V0Z" />
+    </g>
+    <defs>
+      <clipPath id="clip0_413_4245">
+        <rect width="240" height="264" />
+      </clipPath>
+    </defs>
+  </svg>
 </template>
 <style scoped>
-.svg-icon path {
-  @apply fill-white bg-[red];
+.shape1 {
+  @apply fill-current; /* Change to any color you like */
+}
+
+.shape2 {
+  @apply fill-current;
+}
+
+.shape3 {
+  @apply fill-current;
+}
+
+.shape4 {
+  @apply fill-current;
 }
 </style>
