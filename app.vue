a53888c1e67ae7d584554bb605651a0a201bede3
Adeteyah <adeteyah@gmail.com>
2024-12-08 02:12:35 +0000

    Dec 8, 2024, 9:12 AM

--- app.vue
+++ app.vue
@@ -4,12 +4,3 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-<script>
-import Lenis from "lenis";
-const lenis = new Lenis({
-  autoRaf: true,
-});
-lenis.on("scroll", (e) => {
-  console.log(e);
-});
-</script>
