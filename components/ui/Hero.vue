3e4e39d3ea37d05de2ab453cc614c08625a6a5cb
Adeteyah <adeteyah@gmail.com>
2024-12-08 01:59:37 +0000

    Dec 8, 2024, 8:59 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="flex justify-center py-24">
     <h1
-      class="text-4xl leading-relaxed md:text-5xl md:leading-relaxed lg:text-6xl lg:leading-relaxed"
+      class="text-4xl leading-relaxed md:text-5xl md:leading-relaxed lg:text-6xl lg:leading-relaxed blur-[2px] hover:blur-[1px] transition-all duration-500"
     >
       Hi! I'm Dete, a
       <span class="opacity-50 hover:opacity-100 cursor-help">designer</span> +
       <span class="opacity-50 hover:opacity-100 cursor-help">developer</span>
       based in Bandung.<br />
-      I work with businesses to help them in apllying best design a I’ve got you
-      covered. Let’s team up and build something extraordinary!
+      I work with businesses to help them in best design a I’ve got you covered.
+      <NuxtLink to="/contact" class="opacity-50 hover:opacity-100">
+        Let’s team up and build something extraordinary!
+      </NuxtLink>
     </h1>
   </div>
 </template>
