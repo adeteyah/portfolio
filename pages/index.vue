42aee20476cf83265277532caf7155ff50efee10
Adeteyah <adeteyah@gmail.com>
2024-12-08 05:35:57 +0000

    Dec 8, 2024, 12:35 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,29 @@
 <template>
-  <section>
-    <div class="grid lg:grid-cols-3 -mx-4">
-      <div
-        class="px-4 pt-4 hover:bg-gray rounded transition-colors duration-500"
-        v-for="work in workData.slice(0, 3)"
-        :key="work.id"
-      >
-        <NuxtLink class="group no-hover grid" :to="`/work/${work.id}`">
-          <div
-            class="rounded relative w-full h-[400px] p-14 group-hover:p-0 bg-gray transition-all duration-500"
-          >
-            <NuxtImg
-              class="rounded object-cover w-full h-full"
-              :src="work.image"
-              :alt="work.title"
-            />
-          </div>
-          <h3 class="text-xl py-4 font-semibold">{{ work.title }}</h3>
-          <hr class="opacity-10" />
-          <div class="flex justify-between py-4 opacity-50">
-            <p>{{ work.year }}</p>
-            <p>{{ work.year }}</p>
-          </div>
-        </NuxtLink>
-      </div>
-    </div>
-    <div class="flex justify-end">
-      <NuxtLink to="/work">( See all work )</NuxtLink>
+  <section class="grid lg:grid-cols-4 gap-4 lg:gap-8">
+    <div v-for="work in workData.slice(0, 4)" :key="work.id">
+      <nuxt-link class="group no-hover grid" :to="`/work/${work.id}`">
+        <div
+          class="relative bg-gray hover:bg-gray/15 px-8 py-8 flex items-center transition-colors duration-500"
+        >
+          <img
+            class="rounded object-cover w-full h-[240px] md:h-[320px] lg:h-[240px]"
+            :src="work.image"
+            :alt="work.title"
+          />
+        </div>
+        <h3
+          class="text-3xl py-6 font-semibold lg:opacity-50 lg:group-hover:opacity-100 transition-all duration-500"
+        >
+          {{ work.title }}
+        </h3>
+        <hr class="opacity-10" />
+        <div
+          class="flex justify-between py-4 lg:opacity-0 lg:group-hover:opacity-100 transition-all duration-500"
+        >
+          <p>{{ work.service }}</p>
+          <p>{{ work.year }}</p>
+        </div>
+      </nuxt-link>
     </div>
   </section>
 </template>
