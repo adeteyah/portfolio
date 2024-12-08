708a070adfc5b19884c37e23f0e9149da9a8dbac
Adeteyah <adeteyah@gmail.com>
2024-12-08 04:04:29 +0000

    Dec 8, 2024, 11:04 AM

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="grid gap-8 lg:gap-11">
     <UiNavbar />
-    <UiHero />
-    <UiSubtitle />
+    <UiHero :class="route.path === '/' ? 'no-hover' : 'blur-[1px]'" />
+    <UiSubtitle :class="route.path === '/' ? 'no-hover' : 'blur-[1px]'" />
   </div>
 </template>
+<script setup>
+import { useRoute } from "vue-router";
+const route = useRoute();
+</script>
