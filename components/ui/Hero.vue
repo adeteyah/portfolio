12db5dfc1bc0acdc9cd3a87e63d7cad0fa75ec92
Adeteyah <adeteyah@gmail.com>
2024-12-08 01:46:33 +0000

    Dec 8, 2024, 8:46 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -3,10 +3,11 @@
     <h1
       class="text-4xl leading-relaxed md:text-5xl md:leading-relaxed lg:text-6xl lg:leading-relaxed"
     >
-      My name is Dete I do code to make my design to reality. I don’t just
-      create visuals—I craft experiences that captivate and deliver. Whether
-      it’s sleek designs or flawless functionality, I’ve got you covered. Let’s
-      team up and build something extraordinary!
+      I'm Dete, a designer + developer <br />
+      do code to make my design to reality. I don’t just create visuals—I craft
+      experiences that captivate and deliver. Whether it’s sleek designs or
+      flawless functionality, I’ve got you covered. Let’s team up and build
+      something extraordinary!
     </h1>
   </div>
 </template>
