a8ec96b6b1f8b8032070d0ad02493ab60eaaa3f6
Adeteyah <adeteyah@gmail.com>
2024-12-08 01:08:25 +0000

    Dec 8, 2024, 8:08 AM

--- components/ui/CurtainEffect.vue.vue
+++ components/ui/CurtainEffect.vue.vue
@@ -1,70 +1,32 @@
 <template>
-  <div ref="canvasContainer" class="canvas-container">
-    <!-- Add your elements with data-planes here -->
-    <div class="plane" data-curtains>
-      <div class="plane-content">
-        <img
-          src="https://assets.awwwards.com/awards/submissions/2024/02/65c3a43c2f798911476445.jpg"
-          alt="Curtain effect"
-        />
-      </div>
-    </div>
+  <div id="canvas"></div>
+  <!-- div used to create our plane -->
+  <div class="plane">
+    <!-- image that will be used as texture by our plane -->
+    <img
+      src="https://assets.awwwards.com/awards/submissions/2024/02/65c3a43c2f798911476445.jpg"
+      crossorigin=""
+    />
   </div>
 </template>
 
-<script>
-import { Curtains } from "curtainsjs";
-
-export default {
-  data() {
-    return {
-      curtains: null,
-    };
-  },
-  mounted() {
-    // Initialize Curtain.js
-    this.curtains = new Curtains({
-      container: this.$refs.canvasContainer,
-    });
-
-    // Example of adding planes to the scene
-    this.curtains.onContextLost(() => {
-      this.curtains.restoreContext();
-    });
-
-    this.curtains.onError(() => {
-      console.error("Curtains.js could not be initialized.");
-    });
-
-    // Define planes and effects
-    this.initPlanes();
-  },
-  beforeDestroy() {
-    if (this.curtains) {
-      this.curtains.dispose();
-    }
-  },
-  methods: {
-    initPlanes() {
-      const planeElements = document.querySelectorAll(".plane");
-
-      planeElements.forEach((planeElement) => {
-        this.curtains.addPlane(planeElement);
-      });
-    },
-  },
-};
-</script>
-
 <style scoped>
-.canvas-container {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background-color: red;
+#canvas {
+  /* make the canvas wrapper fits the document */
+  position: absolute;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
 }
 .plane {
-  width: 100%;
-  height: auto;
+  /* define the size of your plane */
+  width: 80%;
+  height: 80vh;
+  margin: 10vh auto;
+}
+.plane img {
+  /* hide the img element */
+  display: none;
 }
 </style>
