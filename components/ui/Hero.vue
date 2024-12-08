0c273edaa2a3c25cce2f402f7a80e229dff40730
Adeteyah <adeteyah@gmail.com>
2024-12-08 00:45:13 +0000

    Dec 8, 2024, 7:45 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -3,12 +3,10 @@
     <h1
       class="text-4xl leading-relaxed md:text-5xl md:leading-relaxed lg:text-6xl lg:leading-relaxed"
     >
-      I’m Dete, the rare mix of a code wizard and a design powerhouse. I don’t
-      just create visuals—I craft experiences that captivate and deliver.
-      Whether it’s sleek designs or flawless functionality, I’ve got you
-      covered. Let’s team up and build something extraordinary!
-      <span class="font-semibold">DESIGNER</span> &
-      <span class="font-semibold">DEVELOPER</span>
+      I’m Dete, and I code to make my design to reality. I don’t just create
+      visuals—I craft experiences that captivate and deliver. Whether it’s sleek
+      designs or flawless functionality, I’ve got you covered. Let’s team up and
+      build something extraordinary!
     </h1>
   </div>
 </template>
