4980cdcb663f3bec96ac205d535449dc58f1b8e3
Adeteyah <adeteyah@gmail.com>
2024-12-08 03:26:21 +0000

    Dec 8, 2024, 10:26 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -3,9 +3,10 @@
     <h1
       class="text-4xl leading-relaxed md:text-5xl md:leading-relaxed lg:text-6xl lg:leading-relaxed blur-[1px] hover:blur-[0.5px] transition-all duration-500"
     >
-      Adit <span class="text-base align-top">(Dete)</span> is a
-      <span class="opacity-50 hover:opacity-100 cursor-help">designer</span> -
-      <span class="opacity-50 hover:opacity-100 cursor-help">developer</span>
+      Hi, I'm Adit <span class="text-2xl align-top">(Dete)</span>. I designs and
+      builds
+      <span class="opacity-50 hover:opacity-100 cursor-help">websites</span> -
+      <span class="opacity-50 hover:opacity-100 cursor-help">app</span>
       based in Bandung. I create stuff to prioritize user comfort.
       <NuxtLink to="/contact" class="opacity-50 hover:opacity-100">
         Let’s team up and build something extraordinary!
