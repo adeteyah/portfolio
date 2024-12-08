4dd21eb4b51be0576750b427bdc734f62e04f255
Adeteyah <adeteyah@gmail.com>
2024-12-08 03:20:26 +0000

    Dec 8, 2024, 10:20 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -4,11 +4,10 @@
       class="text-4xl leading-relaxed md:text-5xl md:leading-relaxed lg:text-6xl lg:leading-relaxed blur-[1px] hover:blur-[0.5px] transition-all duration-500"
     >
       Hi! I'm Dete, a
-      <span class="opacity-50 hover:opacity-100 cursor-help">designer</span> +
+      <span class="opacity-50 hover:opacity-100 cursor-help">designer</span> and
+      a
       <span class="opacity-50 hover:opacity-100 cursor-help">developer</span>
-      based in Bandung.<br />
-      I partner with businesses to bring bold ideas to life through innovative
-      design and seamless development.
+      based in Bandung. I create stuff to prioritize user comfort.
       <NuxtLink to="/contact" class="opacity-50 hover:opacity-100">
         Let’s team up and build something extraordinary!
       </NuxtLink>
