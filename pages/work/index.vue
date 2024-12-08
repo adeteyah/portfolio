5839a60d6b4b1387aae81c49030f957de5d648a5
Adeteyah <adeteyah@gmail.com>
2024-12-08 05:30:04 +0000

    Dec 8, 2024, 12:30 PM

--- pages/work/index.vue
+++ pages/work/index.vue
@@ -17,11 +17,13 @@
           {{ work.title }}
         </h3>
         <hr class="opacity-10" />
-        <div
-          class="flex justify-between py-4 lg:opacity-0 lg:group-hover:opacity-100 transition-all duration-500"
-        >
+        <div class="flex justify-between py-4 opacity-25">
           <p>{{ work.year }}</p>
-          <p>{{ work.year }}</p>
+          <p
+            class="lg:opacity-0 lg:group-hover:opacity-100 transition-all duration-500"
+          >
+            {{ work.year }}
+          </p>
         </div>
       </nuxt-link>
     </div>
