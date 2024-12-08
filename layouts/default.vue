89516fa70ff6161c8fc048eb821a190519b9295b
Adeteyah <adeteyah@gmail.com>
2024-12-08 05:27:28 +0000

    Dec 8, 2024, 12:27 PM

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="px-8 md:px-24 lg:px-40 py-11">
-    <transition name="page" mode="out-in">
+    <transition name="noizer" mode="out-in">
       <div key="$route.path" class="grid gap-4 lg:gap-11">
         <div class="noise" v-if="isTransitioning" />
         <AppHeader />
