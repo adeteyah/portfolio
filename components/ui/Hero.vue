bc09775f70d3cea4c807a48a7f38996f5fb0567f
Adeteyah <adeteyah@gmail.com>
2024-12-08 01:50:23 +0000

    Dec 8, 2024, 8:50 AM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -3,7 +3,10 @@
     <h1
       class="text-4xl leading-relaxed md:text-5xl md:leading-relaxed lg:text-6xl lg:leading-relaxed"
     >
-      I'm Dete, a designer + developer <br />
+      I'm Dete, a
+      <span class="opacity-50 hover:opacity-100 cursor-pointer">designer</span>
+      + developer
+      <br />
       I’ve got you covered. Let’s team up and build something extraordinary!
     </h1>
   </div>
