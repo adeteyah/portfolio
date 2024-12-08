f66b9d073640555f4af58ce73fa982d2b71656f7
Adeteyah <adeteyah@gmail.com>
2024-12-08 04:07:34 +0000

    Dec 8, 2024, 11:07 AM

--- components/app/Header.vue
+++ components/app/Header.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="grid gap-8 lg:gap-11">
     <UiNavbar />
-    <UiHero :class="route.path === '/' ? 'no-hover' : 'blur-[1px]'" />
-    <UiSubtitle :class="route.path === '/' ? 'no-hover' : 'blur-[1px]'" />
+
+    <!-- Conditionally render based on the route -->
+    <UiHero v-if="isHome" />
+    <UiSubtitle v-if="isHome" />
   </div>
 </template>
+
 <script setup>
 import { useRoute } from "vue-router";
+
 const route = useRoute();
+
+// Determine if the current route is the home page ('/')
+const isHome = route.path === "/";
 </script>
