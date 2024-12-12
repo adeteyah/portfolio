ba210069e97060a84ba137a3c065705a6c01c602
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 17:26:37 +0000

    Dec 13, 2024, 12:26 AM

--- components/icons/AR.vue
+++ components/icons/AR.vue
@@ -5,10 +5,9 @@
     xmlns="http://www.w3.org/2000/svg"
   >
     <g clip-path="url(#clip0_413_4245)">
-      <path d="M96 264H144L48 0H0L96 264Z" />
       <path d="M144 132H96L192 264H240L144 132Z" />
       <path d="M96 0H48L144 132H192L96 0Z" />
-      <path d="M0 0H48V264H0V0Z" />
+      <path d="M144 264H96L48 132V264H0V0H48L144 264Z" />
     </g>
     <defs>
       <clipPath id="clip0_413_4245">
@@ -19,6 +18,6 @@
 </template>
 <style scoped>
 path {
-  @apply fill-current;
+  @apply opacity-10 hover:opacity-100 transition-all duration-500;
 }
 </style>
