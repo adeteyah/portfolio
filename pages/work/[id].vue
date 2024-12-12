3424a56eafdb4e118965684f12007c138b23c38f
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 17:48:51 +0000

    Dec 13, 2024, 12:48 AM

--- pages/work/[id].vue
+++ pages/work/[id].vue
@@ -1,5 +1,55 @@
 <template>
   <section>
-    <img src="" alt="" />
+    <div class="grid md:grid-cols-2 lg:grid-cols-2 gap-y-16 gap-x-8">
+      <div
+        v-for="work in shouldSlice ? workData.slice(0, 2) : workData"
+        :key="work.id"
+      >
+        <nuxt-link class="group grid gap-8" :to="`/work/${work.id}`">
+          <div
+            class="relative bg-gray lg:group-hover:bg-gray/15 flex items-center transition-all duration-500 aspect-video px-12 pt-16 lg:group-hover:pt-8 shadow-inner"
+          >
+            <img
+              class="object-cover w-full h-full rounded-t-md"
+              :src="work.image"
+              :alt="work.title"
+            />
+          </div>
+          <div class="grid gap-4">
+            <h3
+              class="text-3xl font-semibold lg:opacity-50 lg:group-hover:opacity-100 transition-all duration-500"
+            >
+              {{ work.title }}
+            </h3>
+            <hr class="opacity-10" />
+            <div
+              class="flex justify-between opacity-25 lg:group-hover:opacity-100 transition-all duration-500"
+            >
+              <p>{{ work.service }}</p>
+              <p>{{ work.year }}</p>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
+    </div>
+    <AtomCallToAction class="mt-12" v-if="shouldSlice">
+      <NuxtLink to="/work">See all work &rarr;</NuxtLink>
+    </AtomCallToAction>
   </section>
 </template>
+
+<script setup>
+import { useRoute } from "vue-router";
+import work from "~/assets/data/work.js";
+
+const route = useRoute();
+const props = defineProps({
+  shouldSlice: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+// Example filter to show only relevant data based on route
+const workData = work.filter((item) => item.category === route.params.category);
+</script>
