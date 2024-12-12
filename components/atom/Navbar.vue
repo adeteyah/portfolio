f862e43b2d073fc84434c785122e1d4b28b38435
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 14:21:54 +0000

    Dec 12, 2024, 9:21 PM

--- components/atom/Navbar.vue
+++ components/atom/Navbar.vue
@@ -1,10 +1,6 @@
 <template>
   <nav class="relative flex justify-between md:text-base">
-    <NuxtLink
-      data-value="digital world"
-      to="/"
-      class="group font-semibold hover:no-underline"
-    >
+    <NuxtLink to="/" class="group font-semibold hover:no-underline">
       A<span
         class="lg:opacity-0 group-hover:opacity-100 transition-opacity duration-500"
         >ditiya</span
@@ -16,21 +12,18 @@
     </NuxtLink>
     <div class="flex gap-4 md:gap-5 lg:gap-6">
       <NuxtLink
-        data-value="Work"
         to="/work"
         :class="route.path === '/work' ? 'no-hover' : 'blur-[1px]'"
       >
         Work
       </NuxtLink>
       <NuxtLink
-        data-value="About"
         to="/about"
         :class="route.path === '/about' ? 'no-hover' : 'blur-[1px]'"
       >
         About
       </NuxtLink>
       <NuxtLink
-        data-value="Contact"
         to="/contact"
         :class="route.path === '/contact' ? 'no-hover' : 'blur-[1px]'"
       >
