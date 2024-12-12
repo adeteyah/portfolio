2ef2c59a8248ce90ac0064650b9f214fb5c89efd
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 13:39:43 +0000

    Dec 12, 2024, 8:39 PM

--- components/section/Work.vue
+++ components/section/Work.vue
@@ -1,35 +1,53 @@
 <template>
-  <section class="grid lg:grid-cols-3 gap-y-16 gap-x-8">
-    <div v-for="work in workData" :key="work.id">
-      <nuxt-link class="group no-hover grid gap-8" :to="`/work/${work.id}`">
-        <div
-          class="relative bg-gray lg:group-hover:bg-gray/15 flex items-center transition-all duration-500 aspect-video px-12 pt-16 lg:group-hover:pt-6"
-        >
-          <img
-            class="object-cover w-full h-full rounded-t-lg"
-            :src="work.image"
-            :alt="work.title"
-          />
-        </div>
-        <div class="grid gap-4">
-          <h3
-            class="text-3xl font-semibold lg:opacity-50 lg:group-hover:opacity-100 transition-all duration-500"
-          >
-            {{ work.title }}
-          </h3>
-          <hr class="opacity-10" />
+  <section>
+    <div class="grid lg:grid-cols-3 gap-y-16 gap-x-8">
+      <div
+        v-for="work in shouldSlice ? workData.slice(0, 6) : workData"
+        :key="work.id"
+      >
+        <nuxt-link class="group no-hover grid gap-8" :to="`/work/${work.id}`">
           <div
-            class="flex justify-between opacity-25 lg:group-hover:opacity-100 transition-all duration-500"
+            class="relative bg-gray lg:group-hover:bg-gray/15 flex items-center transition-all duration-500 aspect-video px-12 pt-16 lg:group-hover:pt-6"
           >
-            <p>{{ work.service }}</p>
-            <p>{{ work.year }}</p>
+            <img
+              class="object-cover w-full h-full rounded-t-lg"
+              :src="work.image"
+              :alt="work.title"
+            />
           </div>
-        </div>
-      </nuxt-link>
+          <div class="grid gap-4">
+            <h3
+              class="text-3xl font-semibold lg:opacity-50 lg:group-hover:opacity-100 transition-all duration-500"
+            >
+              {{ work.title }}
+            </h3>
+            <hr class="opacity-10" />
+            <div
+              class="flex justify-between opacity-25 lg:group-hover:opacity-100 transition-all duration-500"
+            >
+              <p>{{ work.service }}</p>
+              <p>{{ work.year }}</p>
+            </div>
+          </div>
+        </nuxt-link>
+      </div>
     </div>
+    <UiCallToAction>
+      <NuxtLink to="/work">All work</NuxtLink>
+    </UiCallToAction>
   </section>
 </template>
+
 <script setup>
 import work from "~/assets/data/work.js";
+
+// Props to toggle slicing
+const props = defineProps({
+  shouldSlice: {
+    type: Boolean,
+    default: true, // Default is true to maintain current behavior
+  },
+});
+
 const workData = work;
 </script>
