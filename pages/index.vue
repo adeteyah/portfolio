16aaeb2069bd8af10c0da7a6b197414cf2a58402
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 13:29:06 +0000

    Dec 12, 2024, 8:29 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,9 @@
 <template>
   <UiHero>
-    I'm a designer & developer based in Bandung. I make 0 &rarr; 1 in this
-    website industry.
-    <span class="text-sm md:text-xl lg:text-2xl align-top">(Dete)</span> a
-    multi-disciplinary based in Bandung. Through teamwork and exploration, I
-    weave tech and design to craft
-    <span class="opacity-50 hover:opacity-100 cursor-help">app</span> —
-    <span class="opacity-50 hover:opacity-100 cursor-help">websites</span
-    >.</UiHero
-  >
+    I'm a designer & developer based in Bandung. I help people do 0 &rarr; 1 in
+    design or building an app or website.
+    <span class="text-sm md:text-xl lg:text-2xl align-top">(More)</span>
+  </UiHero>
   <UiSubtitle />
   <SectionWorkSliced />
 </template>
