80462460d6985b7e0d9802e27a676f8e9a1efdf1
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 23:50:24 +0000

    Dec 13, 2024, 6:50 AM

--- pages/work/[id].vue
+++ pages/work/[id].vue
@@ -1,28 +1,23 @@
 <template>
   <section>
-    <div v-if="currentWork" class="grid gap-8">
-      <div>
-        <div class="relative aspect-video shadow-inner">
-          <img
-            class="object-cover w-full h-full rounded-t-md"
-            :src="currentWork.image"
-            :alt="currentWork.title"
-          />
-        </div>
-        <div class="flex flex-col lg:flex-row justify-between gap-2 mt-8">
-          <h3 class="text-xl font-semibold">
-            {{ currentWork.title }}
-          </h3>
-          <p class="text-3xl font-regular opacity-50">{{ currentWork.year }}</p>
-          <p class="text-xl font-regular opacity-50">
-            {{ currentWork.service }}
-          </p>
-          <hr class="opacity-10" />
-        </div>
+    <div>
+      <div class="relative aspect-video shadow-inner">
+        <img
+          class="object-cover w-full h-full rounded-t-md"
+          :src="currentWork.image"
+          :alt="currentWork.title"
+        />
       </div>
-    </div>
-    <div v-else>
-      <p>Work not found.</p>
+      <div class="flex flex-col lg:flex-row justify-between gap-2 mt-8">
+        <h3 class="text-xl font-semibold">
+          {{ currentWork.title }}
+        </h3>
+        <p class="text-3xl font-regular opacity-50">{{ currentWork.year }}</p>
+        <p class="text-xl font-regular opacity-50">
+          {{ currentWork.service }}
+        </p>
+        <hr class="opacity-10" />
+      </div>
     </div>
   </section>
 </template>
