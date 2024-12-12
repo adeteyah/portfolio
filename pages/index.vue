9668b30e7558639992166203ccd50932859eaeaa
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 14:56:18 +0000

    Dec 12, 2024, 9:56 PM

--- pages/index.vue
+++ pages/index.vue
@@ -8,4 +8,13 @@
   </AtomHero>
   <AtomSubtitle />
   <OrganismWork />
+  <AtomText>
+    <template #title>About</template>
+    <template #text>
+      I'm a designer 🎨 and developer 🧑‍💻 dedicated to creating user-friendly
+      solutions that prioritize comfort, simplicity, and seamless functionality
+      in every project. My work spans the entire development lifecycle—from
+      strategic planning to final product launch.
+    </template>
+  </AtomText>
 </template>
