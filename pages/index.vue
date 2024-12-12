c7726d3b3fa58d920a3ada87a66f7b030e3df399
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 13:30:25 +0000

    Dec 12, 2024, 8:30 PM

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <UiHero>
-    I'm a designer & developer based in Bandung. I help people do 0 &rarr; 1 in
-    design or building an app or website.
+    I'm a designer & developer based in Bandung. I help people like you to
+    achieve 0 &rarr; 1 in design or building an app ─ website.
     <span class="text-sm md:text-xl lg:text-2xl align-top">(More)</span>
   </UiHero>
   <UiSubtitle />
