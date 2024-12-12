d6035cffdcaf8967c40556b0ed867e59df086656
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 13:27:54 +0000

    Dec 12, 2024, 8:27 PM

--- components/ui/Hero.vue
+++ components/ui/Hero.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex justify-center py-24">
+  <div class="flex justify-center py-8 md:py-12 lg:py-16">
     <h1
-      class="text-4xl leading-relaxed md:text-5xl md:leading-relaxed lg:text-6xl lg:leading-relaxed blur-[1px] hover:blur-[0.5px] transition-all duration-500"
+      class="text-2xl leading-relaxed md:text-4xl md:leading-relaxed lg:text-6xl lg:leading-relaxed transition-all duration-500"
     >
       <slot></slot>
     </h1>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,8 @@
 <template>
   <UiHero>
-    Hi, I'm Adit <span class="text-2xl align-top">(Dete)</span> a
+    I'm a designer & developer based in Bandung. I make 0 &rarr; 1 in this
+    website industry.
+    <span class="text-sm md:text-xl lg:text-2xl align-top">(Dete)</span> a
     multi-disciplinary based in Bandung. Through teamwork and exploration, I
     weave tech and design to craft
     <span class="opacity-50 hover:opacity-100 cursor-help">app</span> —
