1447abc51d9d3beb982be053510d8200b428ea9f
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 17:06:02 +0000

    Dec 13, 2024, 12:06 AM

--- components/atom/Subtitle.vue
+++ components/atom/Subtitle.vue
@@ -1,28 +1,23 @@
 <template>
   <div class="hidden lg:grid lg:grid-cols-3 opacity-50 gap-2 text-sm">
     <span class="lg:text-left">
-      {{ firstText }}
+      <slot name="firstText" />
     </span>
     <span class="lg:text-center">
-      {{ secondText }}
+      <slot name="secondText" />
     </span>
     <span class="lg:text-right">
-      {{ thirdText }}
+      <slot name="thirdText" />
     </span>
   </div>
   <div class="lg:hidden opacity-50">
-    {{ firstText }}. {{ secondText }}. {{ thirdText }}.
+    <slot name="firstText" />. <slot name="secondText" />.
+    <slot name="thirdText" />.
   </div>
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      firstText: "I design for user comfort",
-      secondText: "I build to bring simplicity to systems",
-      thirdText: "Let’s make your vision come alive!",
-    };
-  },
+  name: "ResponsiveText",
 };
 </script>
