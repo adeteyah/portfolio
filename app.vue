0cf85089cf7324accd266643f2699a80ce2fb4bd
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 23:58:59 +0000

    Dec 13, 2024, 6:58 AM

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
 <template>
   <div class="noise"></div>
   <NuxtLayout>
-    <NuxtPage />
+    <div class="page-content">
+      <NuxtPage :transition="pageTransition" />
+    </div>
   </NuxtLayout>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      pageTransition: {
+        name: "page",
+        mode: "in-out", // Ensures the leave transition finishes before enter
+      },
+    };
+  },
+};
+</script>
