514a3c0e7bf6ae0e61a98223b1582d6af0248a92
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 11:02:37 +0000

    Dec 12, 2024, 6:02 PM

--- components/ui/CallToAction.vue
+++ components/ui/CallToAction.vue
@@ -1,5 +1,10 @@
 <template>
   <section class="flex justify-center">
-    <slot></slot>
+    <div class="cta"><slot></slot></div>
   </section>
 </template>
+<style scoped>
+.cta > * {
+  @apply px-8 py-4 bg-gray rounded-lg;
+}
+</style>
