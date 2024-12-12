1cfa9e8895978b44ef2e69455d39e8e182a62300
Dandi Darojat <adeteyah@gmail.com>
2024-12-12 15:06:56 +0000

    Dec 12, 2024, 10:06 PM

--- components/atom/Text.vue
+++ components/atom/Text.vue
@@ -1,10 +1,8 @@
 <template>
-  <div class="grid gap-11">
-    <div class="indent-28 opacity-50 text-lg md:text-xl lg:text-2xl uppercase">
-      [ <slot name="title" /> ]
-    </div>
+  <div class="grid gap-4 indent-12 md:indent-24 lg:indent-32">
+    <div class="text-lg uppercase opacity-50">[ <slot name="title" /> ]</div>
     <div
-      class="indent-28 text-2xl md:text-3xl lg:text-4xl leading-relaxed md:leading-loose lg:leading-loose"
+      class="text-xl md:text-2xl lg:text-3xl leading-relaxed md:leading-loose lg:leading-loose"
     >
       <slot name="text" />
     </div>
