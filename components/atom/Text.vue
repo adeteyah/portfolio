b013a0ac994fa8bd8c96b8aee2362d9b236f04a0
Dandi Darojat <adeteyah@gmail.com>
2024-12-13 01:34:49 +0000

    Dec 13, 2024, 8:34 AM

--- components/atom/Text.vue
+++ components/atom/Text.vue
@@ -1,6 +1,6 @@
 <template>
   <setion class="grid gap-4 indent-12 md:indent-24 lg:indent-32">
-    <div class="uppercase opacity-50">[ <slot name="title" /> ]</div>
+    <div class="uppercase opacity-50 text-sm">[ <slot name="title" /> ]</div>
     <div class="paragraph">
       <slot name="text" />
     </div>
