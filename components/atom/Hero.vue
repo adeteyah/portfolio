16724db8ea2536f44dd715573413daf0880fbb54
Dandi Darojat <adeteyah@gmail.com>
2024-12-13 01:08:56 +0000

    Dec 13, 2024, 8:08 AM

--- components/atom/Hero.vue
+++ components/atom/Hero.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center py-4">
-    <h1 class="text-lead">
+    <h1 class="lead">
       <slot></slot>
     </h1>
   </div>
--- components/atom/Text.vue
+++ components/atom/Text.vue
@@ -1,7 +1,7 @@
 <template>
   <setion class="grid gap-4 indent-12 md:indent-24 lg:indent-32">
     <div class="uppercase opacity-50">[ <slot name="title" /> ]</div>
-    <div class="text-paragraph">
+    <div class="paragraph">
       <slot name="text" />
     </div>
   </setion>
