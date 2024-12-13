5167e3a6a169429480542f68b833627a0e4bfc35
Dandi Darojat <adeteyah@gmail.com>
2024-12-13 00:24:40 +0000

    Dec 13, 2024, 7:24 AM

--- pages/index.vue
+++ pages/index.vue
@@ -12,4 +12,13 @@
     <template #thirdText> Let’s make your vision come alive! </template>
   </AtomSubtitle>
   <OrganismWork />
+  <AtomText>
+    <template #title>About</template>
+    <template #text>
+      I'm a designer 🎨 and developer 🧑‍💻 dedicated to creating user-friendly
+      solutions that prioritize comfort, simplicity, and seamless functionality
+      in every project. My work spans the entire development lifecycle—from
+      strategic planning to final product launch.
+    </template>
+  </AtomText>
 </template>
