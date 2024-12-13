586072c57f1e6107d073ce2e30f3b7bda17665e8
Aditiya Rachman <adeteyah@gmail.com>
2024-12-13 18:22:19 +0000

    Dec 14, 2024, 1:22 AM

--- layouts/work.vue
+++ layouts/work.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex gap-4">
-    <div class="bg-[red] w-1/3 h-[100svh]">
-      <div class="sticky">Test</div>
+    <div class="bg-[red] w-1/3 flex flex-col justify-between h-[100svh]">
+      <div>Test</div>
+      <div>Test</div>
     </div>
     <div class="bg-[blue] w-2/3">
       <slot />
