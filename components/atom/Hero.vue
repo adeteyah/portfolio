20e17ef5a72170bb848c6b0fde90f46a49fbe49e
Dandi Darojat <adeteyah@gmail.com>
2024-12-13 01:07:46 +0000

    Dec 13, 2024, 8:07 AM

--- components/atom/Hero.vue
+++ components/atom/Hero.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="flex justify-center py-4">
-    <h1
-      class="text-2xl leading-relaxed md:text-4xl md:leading-relaxed lg:text-6xl lg:leading-relaxed transition-all duration-500"
-    >
+    <h1 class="text-lead">
       <slot></slot>
     </h1>
   </div>
--- components/atom/Text.vue
+++ components/atom/Text.vue
@@ -1,9 +1,7 @@
 <template>
   <setion class="grid gap-4 indent-12 md:indent-24 lg:indent-32">
-    <div class="text-lg uppercase opacity-50">[ <slot name="title" /> ]</div>
-    <div
-      class="text-xl md:text-2xl lg:text-3xl leading-relaxed md:leading-loose lg:leading-loose"
-    >
+    <div class="uppercase opacity-50">[ <slot name="title" /> ]</div>
+    <div class="text-paragraph">
       <slot name="text" />
     </div>
   </setion>
