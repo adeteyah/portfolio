e1f217a4470d3dd18a72c58ab4cd238389b6b8b6
Dandi Darojat <adeteyah@gmail.com>
2024-12-13 01:01:38 +0000

    Dec 13, 2024, 8:01 AM

--- pages/index.vue
+++ pages/index.vue
@@ -3,9 +3,6 @@
     <AtomHero>
       I'm a creative designer & developer based in Bandung. I specialize in
       taking projects from 0 &rarr; 1 in design or building an app ─ website.
-      <NuxtLink to="/" class="text-sm md:text-xl lg:text-2xl align-top"
-        >(More)</NuxtLink
-      >
     </AtomHero>
   </section>
   <section>
