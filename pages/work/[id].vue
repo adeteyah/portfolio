03c2d7bebaffb2c948f806e87d705b936bbd5aaa
Aditiya Rachman <adeteyah@gmail.com>
2024-12-13 18:01:45 +0000

    Dec 14, 2024, 1:01 AM

--- pages/work/[id].vue
+++ pages/work/[id].vue
@@ -1,23 +1,21 @@
 <template>
   <div>
-    <div>
-      <div class="relative aspect-video">
-        <img
-          class="object-cover w-full h-full rounded-t-md"
-          :src="currentWork.image"
-          :alt="currentWork.title"
-        />
-      </div>
-      <div class="flex flex-col lg:flex-row justify-between gap-2 mt-8">
-        <h3 class="text-xl font-semibold">
-          {{ currentWork.title }}
-        </h3>
-        <p class="text-3xl font-regular opacity-50">{{ currentWork.year }}</p>
-        <p class="text-xl font-regular opacity-50">
-          {{ currentWork.service }}
-        </p>
-        <hr class="opacity-10" />
-      </div>
+    <div class="relative aspect-video">
+      <img
+        class="object-cover w-full h-full rounded-t-md"
+        :src="currentWork.image"
+        :alt="currentWork.title"
+      />
+    </div>
+    <div class="flex flex-col lg:flex-row justify-between gap-2 mt-8">
+      <h3 class="text-xl font-semibold">
+        {{ currentWork.title }}
+      </h3>
+      <p class="text-3xl font-regular opacity-50">{{ currentWork.year }}</p>
+      <p class="text-xl font-regular opacity-50">
+        {{ currentWork.service }}
+      </p>
+      <hr class="opacity-10" />
     </div>
   </div>
 </template>
