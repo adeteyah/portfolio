008355d6ba0357f98287b848d6826074de882537
Dandi Darojat <adeteyah@gmail.com>
2024-12-13 02:09:16 +0000

    Dec 13, 2024, 9:09 AM

--- layouts/work.vue
+++ layouts/work.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex-row lg:flex h-full min-h-[100svh] gap-8 py-8">
-    <div class="lg:h-full flex-col">
-      <div c>Test</div>
+    <div>
+      <AppHeader />
       <AppFooter />
     </div>
     <div><slot /></div>
