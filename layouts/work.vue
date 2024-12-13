93707db1bb971a8ef03921e25083da0bab7ee0b8
Aditiya Rachman <adeteyah@gmail.com>
2024-12-13 18:15:24 +0000

    Dec 14, 2024, 1:15 AM

--- layouts/work.vue
+++ layouts/work.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="grid grid-cols-2 bg-[yellow]">
-    <div class="bg-[red] w-1/3">
-      <div>Test</div>
-      <div>Test</div>
-      <div>Test</div>
+  <div class="flex gap-4">
+    <div class="bg-[red] w-1/3 h-[100svh]">
+      <div class="sticky">Test</div>
     </div>
     <div class="bg-[blue] w-2/3">
+      <slot />
       <slot />
     </div>
   </div>
