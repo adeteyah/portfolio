bb20c4803d9407eabe7b36f9c0d211fcee45a009
Dandi Darojat <adeteyah@gmail.com>
2024-12-13 02:01:49 +0000

    Dec 13, 2024, 9:01 AM

--- layouts/work.vue
+++ layouts/work.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="flex gap-8 py-8">
-    <div class="self-stretch bg-[red] h-full">
-      <AppHeader />
+  <div class="flex h-full min-h-[100svh] bg-white gap-8 py-8">
+    <div class="flex justify-between h-full">
+      <div c>Test</div>
       <AppFooter />
     </div>
     <div><slot /></div>
