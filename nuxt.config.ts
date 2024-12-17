474a19853414fa30b901f14c95c7e49cf7085f5e
Dandi Darojat <adeteyah@gmail.com>
2024-12-17 06:15:20 +0000

    Dec 17, 2024, 1:15 PM

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,25 +4,19 @@ import { defineNuxtConfig } from "nuxt/config";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: [
-    "@nuxtjs/tailwindcss",
-    "@nuxt/fonts",
-    "@nuxt/image",
-    "@nuxtjs/color-mode",
-    "@nuxt/scripts",
-  ],
+  modules: ["@nuxt/fonts", "@nuxt/image", "@nuxtjs/color-mode"],
   css: ["~/assets/css/main.css"],
   devServer: {
     host: "localhost",
     port: 3000,
     https: false,
   },
-  app: {
-    pageTransition: { name: "fade" },
-    layoutTransition: { name: "fade" },
-  },
+  // app: {
+  //   pageTransition: { name: "fade" },
+  //   layoutTransition: { name: "fade" },
+  // },
   plugins: ["~/plugins/lenis.client.js"],
   experimental: {
-    renderJsonPayloads: false
-  }
+    renderJsonPayloads: false,
+  },
 });
